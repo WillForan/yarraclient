c22a12fba611784475df79eee8c4a64e812e714c
Tobias Block <tobias.block@nyumc.org>
2022-08-23 20:46:09 -0400

    Continued w Diagnostics

--- Diagnostics/yd_test_logserver.cpp
+++ Diagnostics/yd_test_logserver.cpp
@@ -2,6 +2,12 @@
 #include "yd_global.h"
 #include "yd_helper.h"
 #include "../Client/rds_exechelper.h"
+#include "../Netlogger/netlogger.h"
+
+#include <QTcpSocket>
+#include <QNetworkProxy>
+#include <QUrlQuery>
+#include <QNetworkReply>
 
 
 ydTestLogServer::ydTestLogServer() : ydTest()
@@ -67,156 +73,149 @@ void ydTestLogServer::testConnection(QString& issues, QString& results)
     YD_RESULT_STARTSECTION
     YD_ADDRESULT("Testing connectivity...");
 
-//    QString serverPath=ui->logServerAddressEdit->text();
-//    int serverPort=8080;
+    QString serverPath=logServerAddress;
+    int serverPort=8080;
 
-//    // Check if the entered address contains a port specifier
-//    int colonPos=serverPath.indexOf(":");
+    // Check if the entered address contains a port specifier
+    int colonPos=serverPath.indexOf(":");
 
-//    if (colonPos!=-1)
-//    {
-//        int cutChars=serverPath.length()-colonPos;
+    if (colonPos!=-1)
+    {
+        int cutChars=serverPath.length()-colonPos;
 
-//        QString portString=serverPath.right(cutChars-1);
-//        serverPath.chop(cutChars);
+        bool portValid=false;
+        QString portString=serverPath.right(cutChars-1);
+        serverPath.chop(cutChars);
+        int tempPort=portString.toInt(&portValid);
+        if (portValid)
+        {
+            serverPort=tempPort;
+        }
+    }
 
-//        // Convert port string into number and validate
-//        bool portValid=false;
+    QString localHostname="";
+    QString localIP="";
 
-//        int tempPort=portString.toInt(&portValid);
+    // Open socket connection to see if the server is active and to
+    // determine the local IP address used for routing to the server.
+    QTcpSocket socket;
+    socket.setProxy(QNetworkProxy::NoProxy);
+    socket.connectToHost(serverPath, serverPort);
 
-//        if (portValid)
-//        {
-//            serverPort=tempPort;
-//        }
-//    }
+    if (socket.waitForConnected(10000))
+    {
+        YD_ADDRESULT_COLORLINE("Able to connect to LogServer", YD_SUCCESS);
+        localIP=socket.localAddress().toString();
+    }
+    else
+    {
+        YD_ADDRESULT_COLORLINE("Unable to connect to LogServer", YD_CRITICAL);
+        YD_ADDISSUE("Unable to connect to LogServer", YD_CRITICAL);
+        YD_RESULT_ENDSECTION
+        return;
+    }
 
-//    QString localHostname="";
-//    QString localIP="";
+    socket.disconnectFromHost();
 
-//    // Open socket connection to see if the server is active and to
-//    // determine the local IP address used for routing to the server.
-//    QTcpSocket socket;
-//    socket.setProxy(QNetworkProxy::NoProxy);
-//    socket.connectToHost(serverPath, serverPort);
+    YD_ADDRESULT_LINE("Local IP for connection to LogServer: " + localIP);
+    localHostname=NetLogger::dnsLookup(localIP);
 
-//    if (socket.waitForConnected(10000))
-//    {
-//        localIP=socket.localAddress().toString();
-//    }
-//    else
-//    {
-//        output += errorPrefix + "Unable to connect to server.";
-//        error=true;
-//    }
+    if (localHostname.isEmpty())
+    {
+        YD_ADDRESULT_COLORLINE("Unable to resolve local hostname", YD_CRITICAL);
+        YD_ADDRESULT_COLORLINE("Check local DNS server settings", YD_CRITICAL);
+        YD_ADDISSUE("Unable to resolve local hostname", YD_CRITICAL);
+        YD_RESULT_ENDSECTION
+        return;
+    }
 
-//    socket.disconnectFromHost();
+    YD_ADDRESULT_LINE("Local hostname: " + localHostname);
 
-//#ifndef NETLOGGER_DISABLE_DOMAIN_VALIDATION
+    // Lookup the hostname of the log server from the DNS server
+    serverPath=NetLogger::dnsLookup(serverPath);
 
-//    // Lookup the hostname of the local client from the DNS server
-//    if (!error)
-//    {
-//        localHostname=NetLogger::dnsLookup(localIP);
+    if (serverPath.isEmpty())
+    {
+        YD_ADDRESULT_COLORLINE("Unable to resolve LogServer hostname", YD_CRITICAL);
+        YD_ADDRESULT_COLORLINE("Check local DNS server settings", YD_CRITICAL);
+        YD_ADDISSUE("Unable to resolve LogServer hostname", YD_CRITICAL);
+        YD_RESULT_ENDSECTION
+        return;
+    }
 
-//        if (localHostname.isEmpty())
-//        {
-//            output += errorPrefix + "Unable to resolve local hostname.<br /><br />IP = " + localIP;
-//            output += "<br />Check local DNS server settings.";
-//            error=true;
-//        }
-//    }
+    QStringList localHostString =localHostname.toLower().split(".");
+    QStringList serverHostString=serverPath.toLower().split(".");
 
-//    // Lookup the hostname of the log server from the DNS server
-//    if (!error)
-//    {
-//        serverPath=NetLogger::dnsLookup(serverPath);
+    if ((localHostString.count()<2) || (serverHostString.count()<2))
+    {
+        YD_ADDRESULT_COLORLINE("Error resolving hostnames", YD_CRITICAL);
+        YD_ADDRESULT_COLORLINE("Localhost: " + localHostname.toLower(), YD_CRITICAL);
+        YD_ADDRESULT_COLORLINE("LogServer: " + serverHostString.join("."), YD_CRITICAL);
+        YD_ADDISSUE("Error resolving hostnames", YD_CRITICAL);
+        YD_RESULT_ENDSECTION
+        return;
+    }
+    else
+    {
+        if ((localHostString.at(localHostString.count()-1)!=(serverHostString.at(serverHostString.count()-1)))
+           || (localHostString.at(localHostString.count()-2)!=(serverHostString.at(serverHostString.count()-2))))
+        {
+            YD_ADDRESULT_COLORLINE("LogServer not in local domain", YD_CRITICAL);
+            YD_ADDRESULT_COLORLINE("Localhost: " + localHostname.toLower(), YD_CRITICAL);
+            YD_ADDRESULT_COLORLINE("LogServer: " + serverPath.toLower(), YD_CRITICAL);
+            YD_ADDISSUE("LogServer not in local domain", YD_CRITICAL);
+            YD_RESULT_ENDSECTION
+            return;
+        }
+    }
 
-//        if (serverPath.isEmpty())
-//        {
-//            output += errorPrefix + "Unable to resolve server name.<br /><br />";
-//            output += "Check local DNS server settings.";
-//            error=true;
-//        }
-//    }
+    QUrlQuery data;
+    QNetworkReply::NetworkError net_error;
+    int http_status=0;
 
-//    // Compare if the local system and the server are on the same domain
-//    if (!error)
-//    {
-//        QStringList localHostString =localHostname.toLower().split(".");
-//        QStringList serverHostString=serverPath.toLower().split(".");
+    // Create temporary instance for testing the logserver connection
+    NetLogger testLogger;
 
-//        if ((localHostString.count()<2) || (serverHostString.count()<2))
-//        {
-//            output += errorPrefix + "Error resolving hostnames.<br /><br />";
-//            output += "Local host: " + localHostname.toLower() + " ("+localIP+")<br />";
-//            output += "Log server: " + serverHostString.join(".");
-//            error=true;
-//        }
-//        else
-//        {
-//            if ((localHostString.at(localHostString.count()-1)!=(serverHostString.at(serverHostString.count()-1)))
-//               || (localHostString.at(localHostString.count()-2)!=(serverHostString.at(serverHostString.count()-2))))
-//            {
-//                output += errorPrefix + "Server not in local domain.<br /><br />";
-//                output += "Local host: " + localHostname.toLower() + "<br />";
-//                output += "Log server: " + serverPath.toLower() + "<br />";
-//                error=true;
-//            }
-//        }
-//    }
+    // Read the full server path entered into the UI control again. The path returned from the DNS
+    // might differ if an DNS alias is used. In this case, the certificate will be incorrect.
+    serverPath         =logServerAddress;
+    QString apiKey     =logServerAPIKey;
+    QString name       ="ConnectionTest";
+    QString errorString="n/a";
 
-//#endif
-
-//    // Check if the server responds to the test entry point
-//    if (!error)
-//    {
-//        QUrlQuery data;
-//        QNetworkReply::NetworkError net_error;
-//        int http_status=0;
-
-//        // Create temporary instance for testing the logserver connection
-//        NetLogger testLogger;
-
-//        // Read the full server path entered into the UI control again. The path returned from the DNS
-//        // might differ if an DNS alias is used. In this case, the certificate will be incorrect.
-//        serverPath         =ui->logServerAddressEdit->text();
-//        QString apiKey     =ui->logServerAPIKeyEdit->text();
-//        QString name       ="ConnectionTest";
-//        QString errorString="n/a";
-
-//        testLogger.configure(serverPath, EventInfo::SourceType::RDS, name, apiKey, true);
-//        data.addQueryItem("api_key",apiKey);
-//        bool success=testLogger.postData(data, NETLOG_ENDPT_TEST, net_error, http_status, errorString);
-
-//        if (!success)
-//        {
-//            if (http_status==0)
-//            {
-//                output += errorPrefix + "No response from server (Error: "+errorString+").<br /><br />";
-
-//                switch (net_error)
-//                {
-//                case QNetworkReply::NetworkError::SslHandshakeFailedError:
-//                    output += "Is SSL certificate installed / valid?";
-//                    break;
-//                case QNetworkReply::NetworkError::AuthenticationRequiredError:
-//                    output += "Is API key correct?";
-//                    break;
-//                default:
-//                    output += "Is server running? Is port correct?";
-//                    break;
-//                }
-//            }
-//            else
-//            {
-//                output += errorPrefix + "Server rejected request (Status: "+QString::number(http_status)+").<br /><br />Is API key corrent?";
-//            }
-
-//            error=true;
-//        }
-//    }
-
+    testLogger.configure(serverPath, EventInfo::SourceType::RDS, name, apiKey, true);
+    data.addQueryItem("api_key",apiKey);
+    bool success=testLogger.postData(data, NETLOG_ENDPT_TEST, net_error, http_status, errorString);
+    if (!success)
+    {
+        if (http_status==0)
+        {
+            YD_ADDRESULT_COLORLINE("No response from LogServer (Error: "+errorString+")", YD_CRITICAL);
+            switch (net_error)
+            {
+            case QNetworkReply::NetworkError::SslHandshakeFailedError:
+                YD_ADDRESULT_COLORLINE("Is SSL certificate installed / valid?", YD_CRITICAL);
+                break;
+            case QNetworkReply::NetworkError::AuthenticationRequiredError:
+                YD_ADDRESULT_COLORLINE("Is API key correct?", YD_CRITICAL);
+                break;
+            default:
+                YD_ADDRESULT_COLORLINE("Is server running? Is port correct?", YD_CRITICAL);
+                break;
+            }
+            YD_ADDISSUE("No response from LogServer", YD_CRITICAL);
+        }
+        else
+        {
+            YD_ADDRESULT_COLORLINE("LogServer rejected request (Status: "+QString::number(http_status)+")", YD_CRITICAL);
+            YD_ADDRESULT_COLORLINE("Is API key corrent?", YD_CRITICAL);
+            YD_ADDISSUE("LogServer rejected request", YD_CRITICAL);
+        }
+    }
+    else
+    {
+        YD_ADDRESULT_COLORLINE("LogServer responded", YD_SUCCESS);
+    }
 
     YD_RESULT_ENDSECTION
 }
