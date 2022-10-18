3199363d792cad75cea2445f1c676cb94d5df000
Tobias Block <tobias.block@nyumc.org>
2022-10-17 20:42:01 -0400

    Updated version numbers.

--- Client/rds_global.h
+++ Client/rds_global.h
@@ -5,7 +5,7 @@
 #include "rds_runtimeinformation.h"
 
 // Definitions
-#define RDS_VERSION     "0.62b2"
+#define RDS_VERSION     "0.62"
 #define RDS_PASSWORD    "nyc2012"
 #define RDS_DBGPASSWORD "pastrami"
 
