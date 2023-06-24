4e4e749f32c6a762905b66bd979b6a52be1c3309
Tobias Block <tobias.block@nyumc.org>
2023-06-24 03:55:49 -0400

    Added anonymizer support for XA40, XA50, XA51

--- CloudTools/yct_anonymize/main.cpp
+++ CloudTools/yct_anonymize/main.cpp
@@ -4,7 +4,7 @@
 
 #include "../yct_prepare/yct_twix_anonymizer.h"
 
-#define YCT_ANONYMIZER_VER "0.2b9"
+#define YCT_ANONYMIZER_VER "0.2b10"
 
 
 class phiEntry
--- CloudTools/yct_prepare/yct_twix_anonymizer.h
+++ CloudTools/yct_prepare/yct_twix_anonymizer.h
@@ -4,7 +4,7 @@
 #include <QtCore>
 
 
-#define YCT_TWIXANONYMIZER_VER "0.1d"
+#define YCT_TWIXANONYMIZER_VER "0.1e"
 
 
 class yctPatientInformation
